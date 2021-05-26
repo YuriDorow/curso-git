fce45a351017e81080ad0d4e9ad2884b95efea31
YuriDorow <yuridorow@gmail.com>
2021-05-25 21:38:05 -0300

    Projeto teste iniciado com condição simples

---
+++ projeto teste/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("O Yuri é Lindão?: ");
+            string resp = Console.ReadLine();
+
+            if (resp == "sim")
+            {
+                Console.WriteLine("Parbéns você acertou!");
+            }
+            else
+            {
+                Console.WriteLine("Nah! Resposta errada!");
+            }
+        }
+    }
+}
